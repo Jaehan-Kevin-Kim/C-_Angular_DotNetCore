7c108c4864fb8ffb7a1fbef0a2384ef27035a9e3
kevin-modern <webadmin@modernbeauty.com>
2022-02-14 16:03:03 -0700

    Implementing GET all items

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,31 @@
+
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+  // GET / items
+
+  [ApiController]
+  // [Route("[controller]")]
+  [Route("items")]
+  public class ItemsController: ControllerBase
+  {
+    private readonly InMemItemsRepository repository;
+
+  public ItemsController(){
+    repository = new InMemItemsRepository();
+  }
+
+  // GET /items
+  [HttpGet]
+
+  public IEnumerable<Item> GetItems()
+  {
+    var items = repository.GetItems();
+    return items;
+  }
+  }
+}
--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections.Generic;
 using Catalog.Entities;
 
@@ -20,6 +21,8 @@ namespace Catalog.Repositories
     public Item GetItem(Guid id)
     {
       return items.Where(item => item.Id == id).SingleOrDefault();
+      
+      
     }
   }
 }
