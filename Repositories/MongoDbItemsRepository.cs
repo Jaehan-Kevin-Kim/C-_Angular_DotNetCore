bb90d5bb9e6e70e72c95cfa833df486f46c60bed
kevin-modern <webadmin@modernbeauty.com>
2022-02-15 16:32:50 -0700

    Added MongoDB Repository Installed MongoDB Nuget

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,41 @@
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+
+  public class MongoDbItemsRepository : IItemsRepository
+  {
+    public MongoDbItemsRepository()
+    {
+
+    }
+
+    public void CreateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void DeleteItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Item GetItem(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IEnumerable<Item> GetItems()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void UpdateItem(Item item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+}
+
+dotnet add package MongoDB.Driver
